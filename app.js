949a3f768007db278fbab33814f4b6f6170b6f55
PRABHAS VARMA <141707979+PRABHAS-VARMA@users.noreply.github.com>
2023-11-17 18:31:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+function redirectToAnotherPage() {
+    window.location.href = "./gif.html"; }
+var button = document.getElementById("button")
+button.addEventListener('click', redirectToAnotherPage)
